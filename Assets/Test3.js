4649467d5bcba5407b298c8c002113c3c1194e59
Satoshi CHO <admin@cho-design-lab.com>
2012-12-04 23:10:53 +0900

    add new tank

---
+++ Assets/Test3.js
@@ -0,0 +1,15 @@
+#pragma strict
+
+ class Test3 extends Tank{
+ 	function InitializeTank():void{
+ 	}
+ 	
+ 	function UpdateTank():void{
+ 	}
+ 	
+ 	function DiscoverdEnemy(enemyInfo:Array):void{
+	}
+	
+	function DiscoverdBlock(distance:float):void{
+	}
+ }
